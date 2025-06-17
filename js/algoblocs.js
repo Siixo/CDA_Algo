5d1ca57f9e69e228ef33d8cb74c93683ce59bb7b
NicolasKnittel <ohsiixo@gmail.com>
2025-06-17 11:09:48 +0200

    Exercices A1 à A7

--- js/algoblocs.js
+++ js/algoblocs.js
@@ -1,10 +1,81 @@
 //Init 
 setPos(300,300);
-changeColor(color.red);
+changeColor(color.blue);
 faceDown();
 setLineWidth(10);
 
-//Exercice A1
+/* //Exercice A1:
 forward(200);
 faceRight(90);
 forward(100);
+//OK
+ */
+
+/* //Exercice A2:
+faceLeft();
+forward(100);
+faceDown();
+forward(100);
+faceRight();
+forward(200);
+faceUp();
+forward(200); 
+//OK
+*/
+
+/* //Exercice A3:
+faceLeft();
+forward(100);
+faceRight();
+forward(200);
+faceLeft();
+forward(100);
+faceUp();
+forward(100);
+faceDown();
+forward(200);
+//OK */
+
+/* //Exercice A4:
+faceRight();
+forward(100);
+arcLeft(50,180);
+faceLeft();
+forward(50);
+arcRight(50,180);
+forward(100);
+faceLeft();
+//OK */
+
+/* //Exercice 5:
+changeColor(color.blue);
+faceRight();
+arcRight(100,180);
+changeColor(color.red);
+arcRight(100,180);
+arcRight(50,180);
+changeColor(color.blue);
+arcLeft(50,180);
+//OK */
+
+/* //Exercice 6:
+faceUp();
+forward(50);
+up();
+forward(50);
+down();
+forward(100);
+//OK */
+
+//Exercice 7:
+faceDown();
+forward(50);
+arcLeft(50,180);
+forward(50);
+up();
+forward(50);
+down();
+forward(100);
+arcLeft(50,180);
+forward(50);
+
